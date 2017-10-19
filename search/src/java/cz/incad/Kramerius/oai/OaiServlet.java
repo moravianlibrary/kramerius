fa3d6acb21ce2d9bc2d51e3188ce40300d458e9d
xermak00 <alexej.ermak96@gmail.com>
2017-10-19 11:05:22 +0200

    fix import java.io.PrintWriter;

--- search/src/java/cz/incad/Kramerius/oai/OaiServlet.java
+++ search/src/java/cz/incad/Kramerius/oai/OaiServlet.java
@@ -31,6 +31,7 @@ import cz.incad.utils.IKeys;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.EnumSet;
